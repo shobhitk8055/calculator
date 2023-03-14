a7271a6f1d5c6a2e27289ce378246b199c85ed9f
Abhishek gupta <abhishek.pec2015@gmail.com>
2023-03-14 22:11:19 +0530

    all features complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Calculator from "./Calculator/Calculator";
 
 function App() {
+  const [changeFocus, setFocus] = useState(false);
+
   return (
-    <div className="container">
+    <div className="container" onClick={() => setFocus((i) => !i)}>
       <div className="row">
         <div className="col-12 col-sm-4 mx-auto mt-4">
-          <Calculator />
+          <Calculator changeFocus={changeFocus} />
         </div>
       </div>
     </div>
--- src/Calculator/Calculator.tsx
+++ src/Calculator/Calculator.tsx
@@ -1,35 +1,37 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import Button from "./Button/Button";
 
-interface Props {}
+interface Props {
+  changeFocus: boolean;
+}
 
 function Calculator(props: Props) {
-  const {} = props;
+  const { changeFocus } = props;
+
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const [prevNum, setPrevNumber] = useState<number | null>(null);
+  const [prevNum, setPrevNumber] = useState<string | null>(null);
   const [number, setNumber] = useState<string | null>(null);
   const [operation, setCurrentOperation] = useState<string | null>(null);
 
+  //Focus on the input since it has no cursor (it should be always focused)
   const focus = () => {
     inputRef.current?.focus();
   };
 
+  //Remove the leading zeros
+  const rmZero = (num: string) => {
+    const number = +num;
+    return number.toString();
+  };
+
+  //To Focus
   useEffect(() => {
     if (inputRef) {
       focus();
     }
-  }, [inputRef]);
+  }, [inputRef, changeFocus]);
 
-  const numberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const text = number?.toString();
-    if (text && text.length > 9) {
-      e.stopPropagation();
-      return;
-    }
-
-    setNumber(e.target.value);
-  };
-
+  //Click on number from the screen
   const numberClicked = (num: number | string) => () => {
     let text = number?.toString();
     if (text && text.length > 9) {
@@ -44,15 +46,20 @@ function Calculator(props: Props) {
     focus();
   };
 
+  //Click on backspace or from keyboard
   const backspace = () => {
     let text = number?.toString();
     if (text && text?.length > 0) {
       text = text?.substring(0, text.length - 1);
       setNumber(text);
     }
+    if (!number && operation) {
+      setCurrentOperation(null);
+    }
     focus();
   };
 
+  //Set a operation
   const setOperation = (op: string) => () => {
     if (number) {
       const prevNum = number;
@@ -60,25 +67,33 @@ function Calculator(props: Props) {
       if (operation !== null && prevNum !== null) {
         calculate();
       }
-      setPrevNumber(+prevNum);
+      setPrevNumber(prevNum);
+      setCurrentOperation(op);
+    } else if(prevNum){
       setCurrentOperation(op);
     }
+    focus();
   };
 
+  //Clear everything
   const clear = () => {
     setNumber(null);
     setPrevNumber(null);
     setCurrentOperation(null);
+    focus();
   };
 
+  //Calcuation the current operation
   const calculate = () => {
     if (number && prevNum && operation) {
-      const result = eval(`${prevNum}${operation}${number}`);
+      const result = eval(`${rmZero(prevNum)}${operation}${rmZero(number)}`);
       setNumber(result);
       setCurrentOperation(null);
     }
+    focus();
   };
 
+  //When user types from the keyboard
   const keyChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const keycode = e.keyCode;
 
@@ -93,29 +108,37 @@ function Calculator(props: Props) {
     }
 
     // If it is a backspace
-    if (e.keyCode === 8 && number) {
-      setNumber(number.substring(0, number.length - 1));
+    if (keycode === 8) {
+      backspace();
+      return;
     }
 
     // If it is a plus
-    if (keycode === 107 || (e.shiftKey === true && e.keyCode === 187)) {
+    if (keycode === 107 || (e.shiftKey === true && keycode === 187)) {
+      setOperation("+")();
     }
 
     // If it is a minus
-    if (keycode === 109 || (e.shiftKey === true && e.keyCode === 189)) {
+    if (keycode === 109 || keycode === 189) {
+      setOperation("-")();
     }
 
     // If it is a multiply
-    if (keycode === 106 || (e.shiftKey === true && e.keyCode === 56)) {
+    if (keycode === 106 || (e.shiftKey === true && keycode === 56)) {
+      setOperation("*")();
     }
 
     // If it is a divide
     if ([191, 111].includes(keycode)) {
-      console.log(e.key);
+      setOperation("/")();
     }
-    
-    console.log(e);
+
+    // If it is a enter
+    if (keycode === 13 || (e.shiftKey === false && e.keyCode === 187)) {
+      calculate();
+    }
   };
+  
   return (
     <div className="calculator-box shadow rounded border">
       <div className="display-area">
@@ -125,6 +148,7 @@ function Calculator(props: Props) {
           onKeyDown={keyChange}
           ref={inputRef}
           className="w-100 input-field"
+          readOnly
         />
         <p className="operation-test">
           {prevNum} {operation} {number}
